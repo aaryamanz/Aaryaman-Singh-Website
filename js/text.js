f9b3427ad978fb7ab638a628c3ffdc92d1f8f4a0
Aaryaman Singh <aaryamansingh@Aaryamans-MacBook-Pro.local>
2024-05-07 14:32:02 -0400

    Made changes to resume section removed dark mode

--- js/text.js
+++ js/text.js
@@ -1,13 +1,12 @@
-const element = document.getElementById('typing');
-const texts = ['Aaryaman Singh', 'Studying Mechanical Engineering @ UofT', 'Interested in Tech & Entrepreneurship'];
+const texts = ['Aaryaman Singh', 'Studying Mechanical Engineering'];
 let textIndex = 0;
 let charIndex = 0;
 let typingDelay = 150;
 let erasingDelay = 100;
-let newTextDelay = 1000; // Adjusted for readability
-let initialText = "I'm "; // Initial text to always show
+let newTextDelay = 1000;
+let initialText = "I'm ";
+let element = document.getElementById('typing');
 
-// Adjust type function to append rather than replace
 function type() {
   if (charIndex < texts[textIndex].length) {
     element.textContent = initialText + texts[textIndex].substring(0, charIndex + 1);
@@ -18,23 +17,30 @@ function type() {
   }
 }
 
-// Adjust erase function to preserve initial text
 function erase() {
   if (charIndex > 0) {
     element.textContent = initialText + texts[textIndex].substring(0, charIndex - 1);
     charIndex--;
     setTimeout(erase, erasingDelay);
   } else {
-    textIndex++;
-    if (textIndex >= texts.length) textIndex = 0;
-    charIndex = 0; // Reset charIndex for the next text
+    textIndex = (textIndex + 1) % texts.length;
+    charIndex = 0;
     setTimeout(type, typingDelay);
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Start typing with the initial text in place
+  console.log("DOMContentLoaded event triggered");
   element.textContent = initialText;
   setTimeout(type, typingDelay);
 });
 
+document.addEventListener("pageshow", function(event) {
+  console.log("Page show event triggered", event);
+  if (!element || !element.textContent.includes(texts[textIndex])) {
+    console.log("Restarting typing effect");
+    element = document.getElementById('typing');  // Re-fetch the element to avoid any stale references
+    element.textContent = initialText;
+    setTimeout(type, typingDelay);
+  }
+});
